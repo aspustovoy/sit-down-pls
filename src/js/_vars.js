8f7ae300b86cb15925c857ed27cf43dee9b35003
Alexander Pustovoy <aspustovoy@gmail.com>
2023-12-06 21:33:18 +0300

    final commit

--- src/js/_vars.js
+++
@@ -1,9 +0,0 @@
-export default {
-  $windowEl: window,
-  $documentEl: document,
-  $htmlEl: document.documentElement,
-  $bodyEl: document.body,
-  $burger: document.querySelector('.burger'),
-  $nav: document.querySelector('.nav'),
-}
-
--- src/js/_vendor.js
+++
@@ -1,6 +0,0 @@
-import './vendor/focus-visible.js';
-import './vendor/choices.js';
-import './vendor/swiper-bundle.js';
-import './vendor/popper.js';
-import './vendor/tippy-bundle.umd.js';
-import './vendor/nouislider.js';
